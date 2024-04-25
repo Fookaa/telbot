114d8da5022aa6ef24e4e57c8f283cb136691f27
alex <mencti48@gmail.com>
2024-04-25 11:32:55 +0430

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const port = process.env.PORT || 4000;
 
 const token = '6909692960:AAF8bQSsshcz_kA6ufvwPXYfnjrkXNQRwCY';
 const bot = new TelegramBot(token, {polling: true});
@@ -11,3 +18,23 @@ bot.onText('//start/', (msg) => {
 bot.on('polling_error', (error) => {
     console.log(error);
 })
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const message = msg.text;
+    console.log(message);
+});
+
+
+
+// ...
+app.get('/', (req ,res) => {
+    res.send('Hello reza')
+})
+app.listen(port, () => console.log('serveris running'))
+
+
+const webhookUrl = 'https://https://api.render.com/deploy/srv-col0c0md3nmc739mvmhg?key=YqUnMKFx9Is/api/telegram-bot';
+
+bot.setWebHook(webhookUrl);
+
